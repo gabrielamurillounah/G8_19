fcb38e12ff8feefcf03cc3acf4001bed327ff247
Luisunah <93801481+Luisunah@users.noreply.github.com>
2021-11-19 18:36:10 -0600

    Agregado de Facturas

---
+++ Facturas/controller/facturas.php
@@ -0,0 +1,49 @@
+<?php
+   if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { 
+    header('Access-Control-Allow-Origin: *'); 
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS'); 
+    header('Access-Control-Allow-Headers: token, Content-Type'); 
+    header('Access-Control-Max-Age: 1728000'); 
+    header('Content-Length: 0'); 
+    header('Content-Type: text/plain'); 
+    die(); 
+  } 
+    header('Access-Control-Allow-Origin: *');   
+    header('Content-Type: application/json'); 
+
+   require_once("../config/conexion.php");
+   require_once("../models/FACTURAS.php");
+   
+   $facturas = new FACTURAS();
+
+   $body = json_decode(file_get_contents("php://input"), true);
+
+   switch($_GET["op"]){
+
+    case "GetFACTURAS":
+        $datos=$facturas->get_facturas();
+        echo json_encode($datos);
+      break;
+
+      case "GetUno":
+        $datos=$facturas->get_factura($body["ID"]);
+        echo json_encode($datos);
+      break;
+
+      case "insertFactura":
+        $datos=$facturas->insert_factura($body["ID"],$body["NUMERO_FACTURA"],$body["ID_SOCIO"],$body["FECHA_FACTURA"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_VENCIMIENTO"],$body["ESTADO"]);
+        echo json_encode("Factura Agregada");
+      break;
+
+      case "EliminarFacturas":
+        $datos=$facturas->delete_factura($body["ID"]);
+        echo json_encode("Factura eliminada");
+        break;
+      
+      case "ActualizarFactura":
+        $datos=$facturas->actualizar_facturas($body["ID"],$body["NUMERO_FACTURA"],$body["ID_SOCIO"],$body["FECHA_FACTURA"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_VENCIMIENTO"],$body["ESTADO"]);
+        echo json_encode("Factura Actualizada");
+        break;
+   }
+
+?>
---
+++ Facturas/models/FACTURAS.php
@@ -0,0 +1,86 @@
+<?php
+
+    class FACTURAS extends Conectar{
+        
+        public function get_facturas(){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM ma_facturas WHERE ID = '1'" ;
+            $sql=$conectar->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }    
+    
+
+        public function get_factura($ID){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM ma_facturas WHERE ESTADO = '1' AND ID = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $ID);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+    
+
+        public function insert_factura($ID, $NUMERO_FACTURA, $ID_SOCIO, $FECHA_FACTURA,$DETALLE,$SUB_TOTAL,$TOTAL_ISV,$TOTAL,$FECHA_VENCIMIENTO,$ESTADO){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO ma_facturas(ID, NUMERO_FACTURA, ID_SOCIO, FECHA_FACTURA, DETALLE, SUB_TOTAL,TOTAL_ISV, TOTAL, FECHA_VENCIMIENTO, ESTADO)
+            VALUES (?,?,?,?,?,?,?,?,?,?);";
+            $sql=$conectar->prepare($sql);
+            $sql->bindvalue(1, $ID);
+            $sql->bindvalue(2, $NUMERO_FACTURA);
+            $sql->bindValue(3, $ID_SOCIO);
+            $sql->bindValue(4, $FECHA_FACTURA);
+            $sql->bindvalue(5, $DETALLE);
+            $sql->bindvalue(6, $SUB_TOTAL);
+            $sql->bindvalue(7, $TOTAL_ISV);
+            $sql->bindvalue(8, $TOTAL);
+            $sql->bindvalue(9, $FECHA_VENCIMIENTO);
+            $sql->bindvalue(10, $ESTADO);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function delete_factura($ID){
+            $conectar=parent :: conexion();
+            parent::set_names();
+            $sql= "DELETE FROM `g8_19`.`ma_facturas` WHERE (`ID` = ?);";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1,$ID);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function actualizar_facturas($ID, $NUMERO_FACTURA, $ID_SOCIO, $FECHA_FACTURA,$DETALLE,$SUB_TOTAL,$TOTAL_ISV,$TOTAL,$FECHA_VENCIMIENTO,$ESTADO){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="UPDATE ma_facturas SET
+            ID=?, NUMERO_FACTURA=?,
+            ID_SOCIO=?, FECHA_FACTURA=?,
+            DETALLE=?, SUB_TOTAL=?,
+            TOTAL_ISV=?, TOTAL=?,
+            FECHA_VENCIMIENTO=?, ESTADO=?
+            WHERE ID = ?;";
+            $sql=$conectar->prepare($sql);
+            $sql->bindvalue(1, $ID);
+            $sql->bindvalue(2, $NUMERO_FACTURA);
+            $sql->bindValue(3, $ID_SOCIO);
+            $sql->bindValue(4, $FECHA_FACTURA);
+            $sql->bindvalue(5, $DETALLE);
+            $sql->bindvalue(6, $SUB_TOTAL);
+            $sql->bindvalue(7, $TOTAL_ISV);
+            $sql->bindvalue(8, $TOTAL);
+            $sql->bindvalue(9, $FECHA_VENCIMIENTO);
+            $sql->bindvalue(10, $ESTADO);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }    
+    }
+
+
+?>
+
+
+
